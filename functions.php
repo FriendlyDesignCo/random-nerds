573669ce169e6b7998dcb0911e7656274494c0c3
Ryan McCahan <ryan@mccahan.net>
2015-02-27 00:42:23 -0700

    Importing ACF

--- functions.php
+++ functions.php
@@ -14,3 +14,16 @@ function randomNerdsScripts()
   wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array(), null);
 }
 add_action('wp_enqueue_scripts', 'randomNerdsScripts');
+
+/* Advanced Custom Fields PRO */
+add_filter('acf/settings/path', 'my_acf_settings_path');
+function my_acf_settings_path( $path ) {
+    return get_stylesheet_directory() . '/acf/';
+}
+add_filter('acf/settings/dir', 'my_acf_settings_dir');
+function my_acf_settings_dir( $dir ) {
+    return get_stylesheet_directory_uri() . '/acf/';
+}
+//add_filter('acf/settings/show_admin', '__return_false');
+include_once( get_stylesheet_directory() . '/acf/acf.php' );
+/* END ACF */
