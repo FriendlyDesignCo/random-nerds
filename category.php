ef846fcbc2a50e4b60beea89d5e6c2b69b2c6690
Ryan McCahan <ryan@mccahan.net>
2015-03-05 00:01:45 -0700

    Category scaffolding

---
+++ category.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+<?php while (have_posts()): the_post();
+$categories = get_the_category();
+$categoryNames = array(); $categorySlugs = array(); $categoryLinks = array();
+foreach ($categories as $category) {
+  $categoryNames[] = $category->cat_name;
+  $categorySlugs[] = 'category-'.$category->slug;
+  $categoryLinks[] = '<a href="' . get_category_link($category->cat_ID) . '">' . $category->cat_name . '</a>';
+}
+?>
+  <div class="category-header">
+  </div>
+  
+  <article class="<?php if (!is_single()): ?>filterable<?php endif; ?> <?php echo implode(' ', $categorySlugs); ?>">
+    <div class="article">
+      <div class="category-info">
+        # <a href="<?php the_permalink(); ?>" class="grey"><?php the_ID(); ?></a> in <?php the_category(', '); ?>
+      </div>
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <hr class="small">
+      <div class="meta-row text-center pad-bottom">
+        <span class="light-grey">By:</span> <a class="author-link" href="<?php echo get_author_posts_url(get_the_author_ID()); ?>"><?php the_author(); ?></a>
+      </div>
+
+      <div class="article-body">
+        <?php the_content('<div class="read-more hidden"><span>Read More</span> <div class="arrow">&#8594;</div><div class="clearfix"></div></div>'); ?>
+      </div>
+
+      <?php if ($i < ($wp_query->found_posts)): ?>
+        <div class="divider-row"><hr class="divider"></div>
+      <?php endif; ?>
+    </div>
+  </article>
+<?php $i++; endwhile; ?>
+<?php get_footer(); ?>
