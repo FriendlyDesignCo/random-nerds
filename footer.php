e071cfdb065b23ff9c9e4bd9b7a25e1bfe424edd
Ryan McCahan <ryan@mccahan.net>
2015-04-06 01:11:05 -0600

    Remove old code

--- footer.php
+++ footer.php
@@ -539,20 +539,5 @@ $(".fittext").textfill({maxFontPixels: 100});
     });
   </script>
 
-  <?php /* Comment Counts */ ?>
-  <?php if (is_single()): ?>
-    <script type="text/javascript">
-        /* * * CONFIGURATION VARIABLES * * */
-        var disqus_shortname = '<?php echo ot_get_option('disqus_shortname'); ?>';
-        /* * * DON'T EDIT BELOW THIS LINE * * */
-        (function () {
-            var s = document.createElement('script'); s.async = true;
-            s.type = 'text/javascript';
-            s.src = '//' + disqus_shortname + '.disqus.com/count.js';
-            (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-        }());
-    </script>
-  <?php endif; ?>
-
   </body>
 </html>
