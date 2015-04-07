4d1dcf56fcf07b4bd796f68acf07006f4aafdfde
Ryan McCahan <ryan@mccahan.net>
2015-04-06 23:56:43 -0600

    Remove fittext JS

--- footer.php
+++ footer.php
@@ -35,17 +35,7 @@
   var ignoredCategories; var categories = ['gaming','politics','pop-culture','tech'];
   var refilteringTexts = ['Refiltering posts','Reticulating splines','Dynotherms connecting','Refactoring pixelations','Powering atomic batteries'];
   var headerBottom = 100; var pastHeader = false;
-  (function(m){m.fn.textfill=function(r){function f(){a.debug&&"undefined"!=typeof console&&"undefined"!=typeof console.debug&&console.debug.apply(console,arguments)}function s(){"undefined"!=typeof console&&"undefined"!=typeof console.warn&&console.warn.apply(console,arguments)}function p(a,b,e,k,n,g){function d(a,b){var c=" / ";a>b?c=" > ":a==b&&(c=" = ");return c}f("[TextFill] "+a+" { font-size: "+b.css("font-size")+",Height: "+b.height()+"px "+d(b.height(),e)+e+"px,Width: "+b.width()+d(b.width(),
-k)+k+",minFontPixels: "+n+"px, maxFontPixels: "+g+"px }")}function q(a,b,e,k,f,g,d,h){for(p(a,b,f,g,d,h);d<h-1;){var l=Math.floor((d+h)/2);b.css("font-size",l);if(e.call(b)<=k){if(d=l,e.call(b)==k)break}else h=l;p(a,b,f,g,d,h)}b.css("font-size",h);e.call(b)<=k&&(d=h,p(a+"* ",b,f,g,d,h));return d}var a=m.extend({debug:!1,maxFontPixels:40,minFontPixels:4,innerTag:"span",widthOnly:!1,success:null,callback:null,fail:null,complete:null,explicitWidth:null,explicitHeight:null,changeLineHeight:!1},r);f("[TextFill] Start Debug");
-this.each(function(){var c=m(a.innerTag+":visible:first",this),b=a.explicitHeight||m(this).height(),e=a.explicitWidth||m(this).width(),k=c.css("font-size"),n=parseFloat(c.css("line-height"))/parseFloat(k);f("[TextFill] Inner text: "+c.text());f("[TextFill] All options: ",a);f("[TextFill] Maximum sizes: { Height: "+b+"px, Width: "+e+"px }");var g=a.minFontPixels,d=0>=a.maxFontPixels?b:a.maxFontPixels,h=void 0;a.widthOnly||(h=q("Height",c,m.fn.height,b,b,e,g,d));var l=void 0,l=q("Width",c,m.fn.width,
-e,b,e,g,d);a.widthOnly?(c.css({"font-size":l,"white-space":"nowrap"}),a.changeLineHeight&&c.parent().css("line-height",n*l+"px")):(g=Math.min(h,l),c.css("font-size",g),a.changeLineHeight&&c.parent().css("line-height",n*g+"px"));f("[TextFill] Finished { Old font-size: "+k+", New font-size: "+c.css("font-size")+" }");c.width()>e||c.height()>b&&!a.widthOnly?(c.css("font-size",k),a.fail&&a.fail(this),f("[TextFill] Failure { Current Width: "+c.width()+", Maximum Width: "+e+", Current Height: "+c.height()+
-", Maximum Height: "+b+" }")):a.success?a.success(this):a.callback&&(s("callback is deprecated, use success, instead"),a.callback(this))});a.complete&&a.complete(this);f("[TextFill] End Debug");return this}})(window.jQuery);
   ;(function(e){e.fn.visible=function(t,n,r){var i=e(this).eq(0),s=i.get(0),o=e(window),u=o.scrollTop(),a=u+o.height(),f=o.scrollLeft(),l=f+o.width(),c=i.offset().top,h=c+i.height(),p=i.offset().left,d=p+i.width(),v=t===true?h:c,m=t===true?c:h,g=t===true?d:p,y=t===true?p:d,b=n===true?s.offsetWidth*s.offsetHeight:true,r=r?r:"both";if(r==="both")return!!b&&m<=a&&v>=u&&y<=l&&g>=f;else if(r==="vertical")return!!b&&m<=a&&v>=u;else if(r==="horizontal")return!!b&&y<=l&&g>=f}})(jQuery);
-$(".fittext").textfill({maxFontPixels: 100});
-    if ($(window).width() <= 640) {
-      $(".mobile-fittext").textfill({maxFontPixels: 100});
-      setTimeout(function(){$(".mobile-fittext").textfill({maxFontPixels: 100});}, 300);
-    }
     $(function(){
       <?php /* Page header */ ?>
       if ($("body").hasClass('page') || $("body").hasClass('error404')) {
@@ -62,17 +52,10 @@ $(".fittext").textfill({maxFontPixels: 100});
             pageHeader.removeClass('skinny');
             $(".page-header img.fat-only").animate({left:0},300);
           }
-          $(".fittext").textfill({maxFontPixels: 100});
         };
         $(".page-header").resize(pageHeaderBreakpointSet);
         pageHeaderBreakpointSet();
       }
-
-      // Text size filling
-      $(".fittext").textfill({maxFontPixels: 100});
-      $(window).resize(function(){
-        $(".fittext").textfill({maxFontPixels: 100});
-      });
 
       // Form submission
       $("a.send-form").click(function(event){
@@ -147,12 +130,6 @@ $(".fittext").textfill({maxFontPixels: 100});
         $(this).toggleClass('closed');
         $("#content").toggleClass('sidebar-hidden');
         $("#content").toggleClass('sidebar-visible');
-        setTimeout(function(){
-          $(".fittext").textfill({maxFontPixels: 100});
-        }, 100);
-        setTimeout(function(){
-          $(".fittext").textfill({maxFontPixels: 100});
-        }, 525);
       });
       $("#menu-open, #menu-close").click(function(){
         $(this).toggleClass('open');
