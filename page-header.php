a8a8c92bb2dbe1ced2cf2196b8864fc4f48edbc0
Ross Nover <ross@friendlydesign.co>
2015-04-07 02:22:20 -0400

    Single header page edits.

--- page-header.php
+++ page-header.php
@@ -1,8 +1,8 @@
 <div class="page-header">
   <div class="wrap">
-    <h2 class="fittext"><span><?php if (is_404()): ?>Bad news.<?php else: ?><?php the_title(); ?><?php endif; ?></span></h2>
+    <h2 class=""><span><?php if (is_404()): ?>Bad news.<?php else: ?><?php the_title(); ?><?php endif; ?></span></h2>
   </div>
-  <img class="avatars fat-only" src="<?php echo bloginfo('template_url'); ?>/images/page-header.png" style="left:-5px;">
-  <img class="avatars skinny-only" src="<?php echo bloginfo('template_url'); ?>/images/page-header-mobile.png" style="left:-5px;">
+  <!-- <img class="avatars fat-only" src="<?php echo bloginfo('template_url'); ?>/images/page-header.png" style="left:-5px;">
+  <img class="avatars skinny-only" src="<?php echo bloginfo('template_url'); ?>/images/page-header-mobile.png" style="left:-5px;">-->
   <div class="clearfix"></div>
 </div>
