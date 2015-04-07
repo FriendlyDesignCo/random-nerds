bb67b41ee083168efd0a7968c56a1aae7cc6752e
Ryan McCahan <ryan@mccahan.net>
2015-04-06 22:42:49 -0600

    Tweak sync

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -rav --progress --exclude=wp-config.php * root@70.32.112.75:/home/randomnerds/public_html/wp-content/themes/random-nerds
+rsync -rav --progress --exclude=extra-config.php --exclude=wp-config.php * root@70.32.112.75:/home/randomnerds/public_html/wp-content/themes/random-nerds
